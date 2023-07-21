25fb76e82b412f9890f07b1ea5ac703ade57fe83
Greg Wu <wkl761212@hotmail.com>
2023-07-21 00:31:10 -0600

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Greg on 2023-07-21.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
